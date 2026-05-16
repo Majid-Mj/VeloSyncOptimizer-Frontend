27ebef40ac3b1a7907e0ed1450d6caf0f1ee23c1
majid <majidvp501@gmail.com>
2026-05-16 10:25:31 +0530

    refactor(login) : login page ui part changed

--- src/Pages/Auth/RegisterPage.jsx
+++ src/Pages/Auth/RegisterPage.jsx
@@ -195,9 +195,30 @@ const RegisterPage = () => {
               {storeLoading ? 'Creating account...' : 'Create account'}
             </button>
 
-            <div className="terms" style={{ marginTop: '10px' }}>
-              © 2026 VeloSync Optimizer. All rights reserved.
+            <div className="divider">or register with</div>
+            
+            <div className="social-row">
+              <button type="button" className="social-btn">
+                <svg width="18" height="18" viewBox="0 0 18 18">
+                  <path d="M17.64 9.2c0-.637-.057-1.251-.164-1.84H9v3.481h4.844c-.209 1.125-.843 2.078-1.796 2.717v2.258h2.908c1.702-1.567 2.684-3.874 2.684-6.615z" fill="#4285F4"/>
+                  <path d="M9 18c2.43 0 4.467-.806 5.956-2.184L12.048 13.558c-.806.54-1.837.86-3.048.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332A8.997 8.997 0 009 18z" fill="#34A853"/>
+                  <path d="M3.964 10.707c-.18-.54-.282-1.117-.282-1.707s.102-1.167.282-1.707V4.961H.957A8.996 8.996 0 000 9c0 1.452.348 2.827.957 4.039l3.007-2.332z" fill="#FBBC05"/>
+                  <path d="M9 3.58c1.321 0 2.508.454 3.44 1.345l2.582-2.58C13.463.891 11.426 0 9 0A8.997 8.997 0 00.957 4.961L3.964 7.293C4.672 5.166 6.656 3.58 9 3.58z" fill="#EA4335"/>
+                </svg>
+                Google
+              </button>
+              <button type="button" className="social-btn">
+                <svg width="18" height="18" viewBox="0 0 23 23">
+                  <path fill="#f3f3f3" d="M0 0h23v23H0z"/><path fill="#f35325" d="M1 1h10v10H1z"/><path fill="#81bc06" d="M12 1h10v10H12z"/><path fill="#05a6f0" d="M1 12h10v10H1z"/><path fill="#ffba08" d="M12 12h10v10H12z"/>
+                </svg>
+                Microsoft
+              </button>
             </div>
+
+            <div className="terms" style={{ marginTop: '20px' }}>
+              By registering, you agree to our <a href="#">Terms</a> & <a href="#">Privacy</a>.
+            </div>
+
           </form>
         </div>
       </div>
